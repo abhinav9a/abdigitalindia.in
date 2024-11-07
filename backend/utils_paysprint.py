f6b8804fd9fe90f026b1bc201289de0a6e087243
abhinav9a <abhinav.raj.9a@gmail.com>
2024-11-07 22:01:30 +0530

    Commission calculation changes

--- backend/utils_paysprint.py
+++ backend/utils_paysprint.py
@@ -48,47 +48,26 @@ def credit_aeps_commission(request, user_id):
             Wallet2.objects.filter(userAccount=user).update(balance=F('balance') + commission)
             PaySprintCommissionTxn.objects.create(userAccount=user, amount=commission, txn_status="Success", desc=desc, agent_name=agent_name)
 
-        if merchant.userType == 'Retailer':
-            retailer_commission = calculate_commission(amount, commission_charge.retailer_commission, commission_charge.is_percentage)
-            add_commission(merchant, retailer_commission, "AePS Commission", "Self")
+        # Current User Commission
+        retailer_commission = calculate_commission(amount, commission_charge.retailer_commission, commission_charge.is_percentage)
+        add_commission(merchant, retailer_commission, "AePS Commission", "Self")
 
-            if merchant.userManager:
-                distributor = UserAccount.objects.get(id=merchant.userManager)
-                if distributor.userType == 'Distributor':
-                    distributor_commission = calculate_commission(amount, commission_charge.distributor_commission, commission_charge.is_percentage)
-                    add_commission(distributor, distributor_commission, "AePS Commission", merchant.username)
+        # Distributor Commission
+        if merchant.userManager:
+            distributor = UserAccount.objects.get(id=merchant.userManager)
+            if distributor.userType == 'Distributor':
+                distributor_commission = calculate_commission(amount, commission_charge.distributor_commission, commission_charge.is_percentage)
+                add_commission(distributor, distributor_commission, "AePS Commission", merchant.username)
 
-                    if distributor.userManager:
-                        master_distributor = UserAccount.objects.get(id=distributor.userManager)
-                        if master_distributor.userType == 'Master Distributor':
-                            master_distributor_commission = calculate_commission(amount, commission_charge.master_distributor_commission, commission_charge.is_percentage)
-                            add_commission(master_distributor, master_distributor_commission, "AePS Commission", merchant.username)
-                elif distributor.userType == 'Master Distributor':
-                    master_distributor_commission = calculate_commission(amount, commission_charge.master_distributor_commission, commission_charge.is_percentage)
-                    add_commission(distributor, master_distributor_commission, "AePS Commission", merchant.username)
-
-        elif merchant.userType == 'Distributor':
-            retailer_commission = calculate_commission(amount, commission_charge.retailer_commission, commission_charge.is_percentage)
-            distributor_commission = calculate_commission(amount, commission_charge.distributor_commission, commission_charge.is_percentage)
-            total_distributor_commission = retailer_commission + distributor_commission
-            add_commission(merchant, total_distributor_commission, "AePS Commission", "Self")
-
-            if merchant.userManager:
-                master_distributor = UserAccount.objects.get(id=merchant.userManager)
-                if master_distributor.userType == 'Master Distributor':
-                    master_distributor_commission = calculate_commission(amount, commission_charge.master_distributor_commission, commission_charge.is_percentage)
-                    add_commission(master_distributor, master_distributor_commission, "AePS Commission", merchant.username)
-
-        elif merchant.userType == 'Master Distributor':
-            retailer_commission = calculate_commission(amount, commission_charge.retailer_commission, commission_charge.is_percentage)
-            distributor_commission = calculate_commission(amount, commission_charge.distributor_commission, commission_charge.is_percentage)
-            master_distributor_commission = calculate_commission(amount, commission_charge.master_distributor_commission, commission_charge.is_percentage)
-            total_master_commission = retailer_commission + distributor_commission + master_distributor_commission
-            add_commission(merchant, total_master_commission, "AePS Commission", "Self")
-
-        else:
-            messages.error(request, f"Invalid User type: {merchant.userType}", extra_tags="danger")
-            return False
+                # Master Distributor Commission
+                if distributor.userManager:
+                    master_distributor = UserAccount.objects.get(id=distributor.userManager)
+                    if master_distributor.userType == 'Master Distributor':
+                        master_distributor_commission = calculate_commission(amount, commission_charge.master_distributor_commission, commission_charge.is_percentage)
+                        add_commission(master_distributor, master_distributor_commission, "AePS Commission", merchant.username)
+            elif distributor.userType == 'Master Distributor':
+                master_distributor_commission = calculate_commission(amount, commission_charge.master_distributor_commission, commission_charge.is_percentage)
+                add_commission(distributor, master_distributor_commission, "AePS Commission", merchant.username)
 
         return True
 
@@ -102,7 +81,6 @@ def credit_aeps_commission(request, user_id):
         logger.error(f"Error occurred in {__name__}: {str(e)}", exc_info=True)
         messages.error(request, "Something Went Wrong.", extra_tags="danger")
         return False
-
 
 def credit_mini_statement_commission(request, merchant_id):
     try:
